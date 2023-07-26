ade5b28bee53afe2bce0fb1af9f5c87e5e31779f
Nasir <nasirbhatti.nk@gmail.com>
2023-07-26 15:11:24 +0500

    Add trimmerDecorator class to trim the more than ten letters

--- person.rb
+++ person.rb
@@ -11,13 +11,17 @@ class Person < Nameable
     @parent_permission = parent_permission
   end
 
+  def can_use_services?
+    of_age? || parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
-  end
-
-  def can_use_services?
-    of_age? || parent_permission
   end
 end
