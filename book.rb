cc29852245743c58dc7434a2d3ae26cb76070881
Nasir <nasirbhatti.nk@gmail.com>
2023-07-27 13:22:50 +0500

    Add book class

---
+++ book.rb
@@ -0,0 +1,12 @@
+class Book
+  attr_accessor :title, :author
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+  end
+
+  def add_rental(person, date)
+    rental = Rental.new(date, self, person)
+    @rentals.push(rental)
+end
